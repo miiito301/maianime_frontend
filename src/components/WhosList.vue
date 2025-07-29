795fc99654cacf1d20f787cfb1e2900c77147fc1
miiito301 <ikaruga0131@gmail.com>
2025-07-29 17:06:12 +0900

    Add files via upload

---
+++ src/components/WhosList.vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- WhosList.vue -->
+  <form @submit.prevent="fetchReviews" class="search-form">
+    <input v-model="reviewer_name" type="text" placeholder="Enter user name" required />
+    <button type="submit">Get List</button>
+  </form>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const reviewer_name = ref('')
+
+const emits = defineEmits(['submit-username'])
+
+const fetchReviews = () => {
+  emits('submit-username', reviewer_name.value)
+  reviewer_name.value = ''
+}
+</script>
